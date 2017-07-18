1f1535f0f211777aebe3e8353433a9f951579aa9
Mr-GC <dollars1m@hotmail.com>
2017-07-18 13:45:05 +0100

    Major Functionality and Style Update
    properties.components.ts callPropertiesService() amended to store "listing" entries in an array
    properties.components.ts amended to use properties.component.css
    
    properties.components.html updated to display listing items in a list
    
    boostrap css added to index.html
    
    Basic global css declarations added to styles.css

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -4,14 +4,12 @@ import { SharedService } from "./../shared.service";
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styles: []
+  styleUrls: ['properties.component.css']
 })
 export class PropertiesComponent implements OnInit {
 	id_postcode: string = "";
 	id_area: string = "";
-	op_image: string = "";
-	op_address: string = "";
-	op_description: string = "";
+	op_propertiesList = {};
 	constructor(private _sharedService: SharedService) { }
 
 	ngOnInit() {
@@ -21,9 +19,12 @@ export class PropertiesComponent implements OnInit {
 		this._sharedService.findProperties(this.id_postcode, this.id_area)
 		.subscribe(
 		lstresult => { 
-			this.op_image = lstresult["listing"][0]["image_url"];
-			this.op_address = lstresult["listing"][0]["displayable_address"];
-			this.op_description = lstresult["listing"][0]["description"];
+			var items: any[] = [];
+			var arrLength = lstresult["listing"].length;
+			for(var i = 0; i < arrLength; i++){
+				 items.push(lstresult["listing"][i]);
+			};
+			this.op_propertiesList = items;
 		},
 		error => {
 					console.log("Error. The findProperties result JSON value is as follows:");
